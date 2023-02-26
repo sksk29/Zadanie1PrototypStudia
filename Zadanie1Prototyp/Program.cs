9c213b6f7c8f24cc29b3cf590b2c050cc39e2521
janus <janus@LAPTOP-U36ER0VI>
2023-02-26 15:41:47 +0100

    Dodaj pliki projektów.

---
+++ Zadanie1Prototyp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+abstract class Prototype
+{
+    public int Id { get; }
+
+    public Prototype(int id)
+    {
+        Id = id;
+    }
+
+    public abstract Prototype Clone();
+}
+
+class PrototypeX : Prototype
+{
+    public PrototypeX(int id) : base(id)
+    {
+    }
+
+    public override Prototype Clone()
+    {
+        return (Prototype)MemberwiseClone();
+    }
+}
+
+class PrototypeY : Prototype
+{
+    public PrototypeY(int id) : base(id)
+    {
+    }
+
+    public override Prototype Clone()
+    {
+        return (Prototype)MemberwiseClone();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PrototypeX prototypeX1 = new PrototypeX(1);
+        PrototypeX prototypeX2 = (PrototypeX)prototypeX1.Clone();
+
+        PrototypeY prototypeY1 = new PrototypeY(2);
+        PrototypeY prototypeY2 = (PrototypeY)prototypeY1.Clone();
+
+        Console.WriteLine($"prototypeX1.Id = {prototypeX1.Id}, prototypeX2.Id = {prototypeX2.Id}");
+        Console.WriteLine($"prototypeY1.Id = {prototypeY1.Id}, prototypeY2.Id = {prototypeY2.Id}");
+    }
+}
